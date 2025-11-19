0de6978326e481aebd6a6b88c95614d035a8c14e
Asim Rajput <me@asim.pro>
2025-11-19 10:30:44 +0400

    fixed bug

--- src/components/ui/Tipper.jsx
+++ src/components/ui/Tipper.jsx
@@ -11,29 +11,29 @@ export default function Tipper() {
 
   return (
     <div
-      class="mx-auto overflow-hidden bg-[#3a7cfd]/30 border-2 border-[#b8f3ff] rounded-[50px] h-[557px]
+      className="mx-auto overflow-hidden bg-[#3a7cfd]/30 border-2 border-[#b8f3ff] rounded-[50px] h-[557px]
           shadow-[0_0_15px_6px_rgba(184,243,255,0.5)]
           "
     >
-      <div class="md:flex h-full">
-        <div class="p-20 items-center flex flex-col justify-center">
-          <div class="px-28 lg:px-10 md:px-10 sm:px-0 text-4xl font-bold text-center text-[#b8f3ff]">
+      <div className="md:flex h-full">
+        <div className="p-20 items-center flex flex-col justify-center">
+          <div className="px-28 lg:px-10 md:px-10 sm:px-0 text-4xl font-bold text-center text-[#b8f3ff]">
             {tipper.title}
           </div>
-          <p class="mt-10 text-white text-left text-xl leading-relaxed font-normal">
+          <p className="mt-10 text-white text-left text-xl leading-relaxed font-normal">
             There are many people who work hard but are not rewarded enough for
             it. <br /> <br />
             We live in a digital age where a large part of the population
-            doesn’t use cash, so when you want to tip someone who truly deserves
+            doesn't use cash, so when you want to tip someone who truly deserves
             it, things become complicated. <br /> <br />
-            That’s why <b>TipTapp</b> is here. Fast, easy, and secure - by
-            scanning the service provider’s QR code, and with the help of
+            That's why <b>TipTapp</b> is here. Fast, easy, and secure - by
+            scanning the service provider's QR code, and with the help of
             Stripe, you can leave a tip
           </p>
         </div>
-        <div class="md:shrink-0">
+        <div className="md:shrink-0">
           <img
-            class="h-full w-full object-cover md:h-full md:w-full"
+            className="h-full w-full object-cover md:h-full md:w-full"
             src={tipper.image}
             alt="Tips Balance"
           />
